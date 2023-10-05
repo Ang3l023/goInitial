ca7da52e5c4e33aa6de55c82ea565e6f9ffa020a
Angelo Rojas <angelo030701@gmail.com>
2023-10-05 18:30:25 -0500

    Func Go

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"Users/Anyelo/Documentos/cursos/ECommerceGoNext/go/src/Ang3l023/goInitial/variables"
+	"fmt"
 )
 
 func main() {
-	variables.MostrarEnteros()
+	estado, texto := variables.ConviertoATexto(1520)
+	fmt.Println(texto)
+	fmt.Println(estado)
 }
